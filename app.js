b2c2f6931470b6c91c7fe3c4b8a85820b4335784
Jasim-10 <jasimcec2025@gmail.com>
2026-03-11 03:33:51 +0530

    WanderLust

--- app.js
+++ app.js
@@ -24,10 +24,12 @@ app.set("view engine",'ejs');
 app.set("views",path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("hello, I am root")
 });
 
 //Index route
